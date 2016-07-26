98b09d0295870e37a535da717f195f86e1f1f212
momajd <mo.a.majd@gmail.com>
2016-07-25 20:48:44 -0400

    heap

---
+++ heap.rb
@@ -0,0 +1,54 @@
+class Heap
+  def initialize
+    @store = []
+  end
+
+  def min
+    @store.first
+  end
+
+  def insert(val)
+    @store.push(val)
+    heapify_up!
+    val
+  end
+
+  def pop_min
+    popped_val = @store.shift
+    heapify_down!
+    popped_val
+  end
+
+  private
+  def swap!(idx1, idx2)
+    @store[idx1], @store[idx2] = @store[idx2], @store[idx1]
+  end
+
+  def parent_idx(idx)
+    return 0 if idx.zero?
+    (idx - 1) / 2
+  end
+
+  def child_indeces(idx)
+    [2 * idx + 1, 2 * idx + 2].select {|i| i < @store.length}
+  end
+
+  def heapify_up!
+    i = @store.length - 1
+    while @store[i] < @store[parent_idx(i)]
+      swap!(i, parent_idx(i))
+      i = parent_idx(i)
+    end
+  end
+
+  def heapify_down!
+    i = 0
+    min_child_idx = child_indeces(i).min_by {|idx| @store[idx]}
+    while (i <= parent_idx(@store.length - 1) && min_child_idx &&
+      @store[i] > @store[min_child_idx])
+        swap!(i, min_child_idx)
+        i = min_child_idx
+        min_child_idx = child_indeces(i).min_by {|idx| @store[idx]}
+    end
+  end
+end
